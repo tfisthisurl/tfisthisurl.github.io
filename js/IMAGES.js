8e7586c82ea65ae213c4abc6188ec4298bd05d66
tfisthisurl <tfisthisurl@gmail.com>
2024-11-04 14:17:42 +0100

    Update IMAGES.js

--- js/IMAGES.js
+++ js/IMAGES.js
@@ -18,10 +18,10 @@ var IMAGES = [
     {
         title: `<i>i don't know what the fuck is his name</i> is shooting`,
         url: './assets/img/imagesBank/cs-4.jpg'
-    }
+    },
 
     {
         title: `lolololo`,
         url: 'https://imgbox.com/mCeCcNuF'
-}
+    }
 ]
