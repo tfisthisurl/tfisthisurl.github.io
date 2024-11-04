8a606292d990ef5249767dff5dd9ec0f224435c6
tfisthisurl <tfisthisurl@gmail.com>
2024-11-04 14:50:38 +0100

    Update IMAGES.js

--- js/IMAGES.js
+++ js/IMAGES.js
@@ -1,26 +1,6 @@
 var IMAGES = [
     
     {
-        title: `someone shooting`,
-        url: './assets/img/imagesBank/cs-1.jpg'
-    },
-    
-    {
-        title: `another person shooting`,
-        url: './assets/img/imagesBank/cs-2.jpg'
-    },
-
-    {
-        title: `bob shooting`,
-        url: './assets/img/imagesBank/cs-3.jpeg'
-    },
-    
-    {
-        title: `<i>i don't know what the fuck is his name</i> is shooting`,
-        url: './assets/img/imagesBank/cs-4.jpg'
-    },
-
-    {
         title: `Dev1ce at IEM Cologne 2023`,
         url: 'https://images2.imgbox.com/97/5b/mCeCcNuF_o.jpg'
     },
@@ -28,5 +8,46 @@ var IMAGES = [
      {
         title: `Dev1ce at PGL Major Stockholm 2021`,
         url: 'https://images2.imgbox.com/e1/b3/xMNgOV1j_o.jpg'
+    },
+     {
+        title: `Device at EPL S10`,
+        url: 'https://images2.imgbox.com/00/37/G4PrS2SY_o.jpg'
+    },
+
+ {
+        title: `Dev1ce at PGL Major Krakow`,
+        url: 'https://images2.imgbox.com/f3/07/ejqhPbwc_o.jpg'
+    },
+ {
+        title: `Dev1ce at IEM Katowice Major 2019`,
+        url: 'https://images2.imgbox.com/a9/6e/wpHbShCH_o.jpg'
+    },
+ {
+        title: ` Dev1ce at ESL One Cologne 2019`,
+        url: 'https://images2.imgbox.com/87/b1/lIdmFH2c_o.jpg'
+    },
+ {
+        title: `Dev1ce at IEM Sydney 2018`,
+        url: 'https://images2.imgbox.com/0f/df/p9RWjgYc_o.jpg'
+    },
+ {
+        title: `Dev1ce at IEM Sydney 2017`,
+        url: 'https://images2.imgbox.com/0f/f9/xSiQ5xKm_o.jpg'
+    },
+ {
+        title: `Dev1ce at IEM Katowice 2020`,
+        url: 'https://images2.imgbox.com/94/16/e9XrvgKk_o.jpg'
+    },
+ {
+        title: `Dev1ce at IEM Cologne 2021`,
+        url: 'https://images2.imgbox.com/00/37/G4PrS2SY_o.jpg'
+    },
+ {
+        title: `Dev1ce at EPL S8`,
+        url: 'https://images2.imgbox.com/5a/91/CUX79fM9_o.jpg'
+    },
+ {
+        title: `Dev1ce at EPL S4`,
+        url: 'https://images2.imgbox.com/b6/e8/en3zsqYG_o.jpg'
     }
 ]
